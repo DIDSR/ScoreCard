c9e4ff53f045eaddc038d6230e080f565d282250
Alexander R. Webber <Alexander.Webber@fda.hhs.gov>
2026-07-07 10:51:40 -0400

    changing from debug to prod

--- app.py
+++ app.py
@@ -885,4 +885,4 @@ def output_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050, use_reloader=False)
+    app.run(debug=False, host='0.0.0.0', port=5050, use_reloader=False)
